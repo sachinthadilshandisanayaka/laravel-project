212fd2795bb4bbaf44201e571fe85bd560969914
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-07-21 00:10:33 +0530

    updated

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <div class="relative min-h-screen">
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-     <h1>Pizza - {{ $id }} </h1>
+     <h1>Order for {{ $pizza->name }} </h1>
+     <p> type - {{ $pizza->type }} </p>
+     <p> base - {{ $pizza->base }} </p>
     </div>
 </div>
 @endsection
